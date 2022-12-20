7e5abc539402dd4e458059bed02676318ab33a1f
algomachine007 <amaben202phantom@gmail.com>
2022-12-20 19:51:22 +0100

    creating a download pdf util

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import PageLayout from '@/layouts/page-layout';
+import { printPDF } from '@/base/utils/print-pdf';
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <PageLayout>Magnetik</PageLayout>;
+  return (
+    <PageLayout>
+      <div id='root'>
+        Magnetik
+        <h1>Generate PDF</h1>
+        <p>Create a screenshot from the page, and put it in a PDF file.</p>
+        <p style={{ color: 'red' }}>
+          *Open this page in new window and press the button.
+        </p>
+        <button id='print' onClick={() => printPDF('root')}>
+          PRINT
+        </button>
+      </div>{' '}
+    </PageLayout>
+  );
 };
 
 export default IndexPage;

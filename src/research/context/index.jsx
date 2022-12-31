bd2b0cc76d1b98ba31bb06433cb076d46accdf5c
algomachine007 <amaben202phantom@gmail.com>
2022-12-31 09:38:12 +0100

    readme update

---
+++ src/research/context/index.jsx
@@ -0,0 +1,2 @@
+// https://www.codecademy.com/learn/learn-advanced-react/modules/advanced-react-context/cheatsheet
+
---
+++ src/research/ssr.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      src='//code.tidio.co/3dvdazgp0snuzjeyzgfkyom90ji0wp18.js'
+      async
+    ></script>,
+  ]);
+};

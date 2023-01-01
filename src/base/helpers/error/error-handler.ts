2f82ee3245ac4137c3c9a268b8fb75a4376597a7
algomachine007 <amaben202phantom@gmail.com>
2023-01-01 10:51:54 +0100

    ensuring all errors in the handler arent cumpolsory

--- src/base/helpers/error/error-handler.ts
+++ src/base/helpers/error/error-handler.ts
@@ -1,12 +1,10 @@
 type TErrorHandler = {
-  error: {
-    name: string,
-    stack: string,
-    message: string,
-  }
+  name: string,
+  stack: string,
+  message: string,
 }
 
-function handleMessage({ error }: TErrorHandler) {
+function handleMessage(error: Partial<TErrorHandler>) {
   return console.error(
     `The type of error is a ${error.name} and its message is ${error.message}. The stack error is of type ${error.stack}. Simply put the error is ${error}`
   );

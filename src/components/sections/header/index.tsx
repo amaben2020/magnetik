90aa89cdcbbd9cfa991faa6eadcaa837754116af
algomachine007 <amaben202phantom@gmail.com>
2023-01-27 14:03:05 +0100

    styling the dropdown

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -1,31 +1,47 @@
 import LinkComponent from '@/components/elements/link';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import * as styles from './styles.module.scss';
+import { useOnHoverOutside } from "../../../hooks/useOnHoverOutside";
 
 const Header = ({ logo, toggleDarkMode, navItems }: any) => {
   console.log('navItems', navItems);
   const [state, setState] = useState(false)
 
-  const handleShowMenu = (id) => {}
+  const handleShowMenu = (e) => {
+e.target.style.background = 'blue'
+  }
+
+  const dropdownRef = useRef(null); // Create a reference for dropdown container
+  const [isMenuDropDownOpen, setMenuDropDownOpen] = useState(false);
+
+  // Function to close dropdown
+  const closeHoverMenu = () => {
+    setMenuDropDownOpen(false);
+  };
+
+  useOnHoverOutside(dropdownRef, closeHoverMenu); // Call the hook
 
   return (
     <>
     <header className={styles.header}>
-      <ul>
+      <ul ref={dropdownRef}>
       <h4>{logo?.title}</h4>
-      {navItems.map((link) => (
-       <li key={link.title}>
-            <LinkComponent url={link.url} title={link.title} />
-              
-            <div  className={styles.dropdown}>
+      {navItems.map((link, index) => (
+       <li key={link.title} 
+       onMouseOver={() => {
+        if (index === 1) {
+            setMenuDropDownOpen(true)
+        }
+      }}
+       >
+        <LinkComponent url={link.url} title={link.title} />
+     {  <div  className={styles.dropdown}>
               {Array.isArray(link.links?.linkCategory)&& link.links?.linkCategory.map((linkCategory) => {
                 return (
-           
-                     <LinkComponent key={linkCategory.url} url={linkCategory.url} title={linkCategory?.title}/> 
-      
+             <LinkComponent key={linkCategory.url} url={linkCategory.url} title={linkCategory?.title}/> 
                 )
               })}
-                    </div>
+      </div>}
           </li>  
         ))}
       </ul>

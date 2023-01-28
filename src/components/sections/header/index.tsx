bb4804c470670039b933506d977e3866c4f1444a
algomachine007 <amaben202phantom@gmail.com>
2023-01-28 08:43:45 +0100

    fixing the handleClickOutside

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -1,54 +1,75 @@
 import LinkComponent from '@/components/elements/link';
 import React, {useState, useRef} from 'react';
 import * as styles from './styles.module.scss';
-import { useOnHoverOutside } from "../../../hooks/useOnHoverOutside";
+import { useDetectOutsideClick } from "../../../hooks/useDetectOutsideClick";
+import { useEffect } from "react";
+ 
 
 const Header = ({ logo, toggleDarkMode, navItems }: any) => {
-  console.log('navItems', navItems);
-  const [state, setState] = useState(false)
+  const [isActive, setIsActive] = useState(null)
 
-  const handleShowMenu = (e) => {
-e.target.style.background = 'blue'
+  const handleClick = (index) => {
+     if (index > 0){
+      setIsActive(index)
+     }
+  } 
+ 
+  const dropdownRef = useRef(null)
+
+  const handleClose = () => {
+    if (isActive){
+        setIsActive(null)
+    }
   }
 
-  const dropdownRef = useRef(null); // Create a reference for dropdown container
-  const [isMenuDropDownOpen, setMenuDropDownOpen] = useState(false);
+  useEffect(() => {
+    const onClick = (e) => {
+    if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+      setIsActive(null);
+       }
+    }
 
-  // Function to close dropdown
-  const closeHoverMenu = () => {
-    setMenuDropDownOpen(false);
-  };
+    if (isActive) {
+      window.addEventListener("click", onClick);
+    }
 
-  useOnHoverOutside(dropdownRef, closeHoverMenu); // Call the hook
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+   
+  }, [isActive])
 
   return (
-    <>
+ 
     <header className={styles.header}>
-      <ul ref={dropdownRef}>
+      <nav ref={dropdownRef}>
+      <ul>
       <h4>{logo?.title}</h4>
       {navItems.map((link, index) => (
-       <li key={link.title} 
-       onMouseOver={() => {
-        if (index === 1) {
-            setMenuDropDownOpen(true)
-        }
-      }}
-       >
-        <LinkComponent url={link.url} title={link.title} />
-     {  <div  className={styles.dropdown}>
-              {Array.isArray(link.links?.linkCategory)&& link.links?.linkCategory.map((linkCategory) => {
+       <li key={link.title}>
+         <button onClick={() => {
+          if (index === isActive) {
+          setIsActive(null)
+              }else{
+          handleClick(index)
+          }}}>{link.title}</button>
+         {isActive === index && <div className={isActive ? styles.menuActive: styles.menuInActive} >
+              { Array.isArray(link.links?.linkCategory)&& link.links?.linkCategory.map((linkCategory, index) => {
                 return (
-             <LinkComponent key={linkCategory.url} url={linkCategory.url} title={linkCategory?.title}/> 
-                )
-              })}
-      </div>}
+                <nav >
+                  <LinkComponent key={linkCategory.url} url={linkCategory.url} title={linkCategory?.title}/> 
+                </nav>
+                )})}
+        </div>}
           </li>  
         ))}
       </ul>
+      </nav>
+    
       {/* TODO: Implement elastic search with modal for blog posts 🔍 */}
       {toggleDarkMode}
-    </header>
-    </>
+      </header>
+ 
   );
 };
 
